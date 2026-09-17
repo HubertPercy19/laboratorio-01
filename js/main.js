8621863b3b9b87f218f3124a90fd9beccb64f7e4
Hubert <hubert@vicom.mx>
2026-09-17 14:14:52 -0500

    Improvement Mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const darkMode = document.querySelector('#dark-mode-toggle');
 const bodyContent = document.querySelector('body');
 
+const navbar = document.querySelector('.header__nav__bar');
+const navContent = document.querySelector('.header__nav__list__content');
 darkMode.addEventListener('click', (e) => {
     bodyContent.classList.toggle('dark-mode');
+})
+
+navbar.addEventListener('click', (e) => {
+    const navBarValue = e.target.parentElement;
+    navBarValue.classList.toggle('active');
+    navBarValue.parentElement.querySelector('.header__nav__list__content').classList.toggle('active');
 })
